19984c95de22418046e6256054078274fd856b3b
Eva González <evagonzalez.tic@gmail.com>
2022-08-24 12:17:43 +0200

    exercise3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import "./App.css";
 import Escena from "./components/escena/Escena.js";
+import { Rectangulo } from "./styled";
 
 function App() {
   return (
     <div className="App">
-      <Escena text="El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial." />
-      <Escena text="Sentia curiositat per l'interior de la nau i es va posar a inspeccionar-la. Va arribar a una sala amb dues portes." />
-      <Escena text="L'heroi va decidir travessar la porta que el portava a casa." />
-      <Escena text="Mentrestant, altres herois no van tenir tanta sort en la seva elecció..." />
+      <Rectangulo>
+        <Escena text="El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial." />{" "}
+      </Rectangulo>
+      <Rectangulo>
+        <Escena text="Sentia curiositat per l'interior de la nau i es va posar a inspeccionar-la. Va arribar a una sala amb dues portes." />
+      </Rectangulo>
+      <Rectangulo>
+        <Escena text="L'heroi va decidir travessar la porta que el portava a casa." />
+      </Rectangulo>
+      <Rectangulo>
+        <Escena text="Mentrestant, altres herois no van tenir tanta sort en la seva elecció..." />
+      </Rectangulo>
     </div>
   );
 }
---
+++ src/styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Rectangulo = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  text-align: center;
+  margin: 20px;
+  border: 1.5px solid;
+  border-radius: 40px;
+  font-size: 18px;
+`;
